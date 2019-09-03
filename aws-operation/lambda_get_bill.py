9779ab2076c7d86e7b7dab9ac0892b79f6c0c9eb
Keys <otegami@devel.keys.jp>
2019-09-03 16:26:18 +0900

    add comulative stat mode

--- aws-operation/lambda_get_bill.py
+++ aws-operation/lambda_get_bill.py
@@ -6,19 +6,28 @@ import urllib.request
 from collections import OrderedDict
 
 
-def notify_to_slack(cost, target_date):
+def notify_to_slack(cost, target_date, stat_type):
   if not os.environ.get('WEBHOOK'):
     return
   items = list()
   for k, v in sorted(cost.items()):
     items.append('{}: {:.6f}'.format(k, float(v)))
-  msg = {
-    "text": "*{} {} daily cost:*\n\n{}".format(
-      os.environ.get('SYSTEM_ENV', ''),
-      target_date,
-      '\n'.join(items)
-    )
-  }
+  sysenv = os.environ.get('SYSTEM_ENV', '')
+  if stat_type == 'daily':
+    msg = {
+      "text": "*{} {} daily cost:*\n\n{}".format(
+        sysenv,
+        target_date,
+        '\n'.join(items)
+      )
+    }
+  elif stat_type == 'month_cumulative':
+    msg = {
+      "text": "*{}: monthly comulative cost:*\n\n{}".format(
+        sysenv,
+        '\n'.join(items)
+      )
+    }
   posting_data = ("payload=" + json.dumps(msg)).encode('utf-8')
   request = urllib.request.Request(
     os.environ.get('WEBHOOK'),
@@ -39,19 +48,32 @@ def normalize_result(resp):
       result[service_name] = cost
   return result
 
-def get_time_period():
+def get_1st_day_of_this_month(tz):
+  dt_now = datetime.datetime.now(tz)
+  dt_1st_day = datetime.datetime(
+    dt_now.year, dt_now.month, 1, 0, 0, 0, 0
+  )
+  return dt_1st_day
+
+def get_time_period(stat_type):
   tz_jst = datetime.timezone(datetime.timedelta(hours=9))
-  dt_end = datetime.datetime.now(tz_jst)
-  dt_start = dt_end - datetime.timedelta(days=1)
+  if stat_type == 'daily':
+    dt_end = datetime.datetime.now(tz_jst)
+    dt_start = dt_end - datetime.timedelta(days=1)
+  elif stat_type == 'month_cumulative':
+    dt_end = datetime.datetime.now(tz_jst)
+    dt_start = datetime.datetime(
+      dt_end.year, dt_end.month, 1, 0, 0, 0, 0
+    )
   time_period = OrderedDict()
-  # time_period = dict()
   time_period['Start'] = dt_start.strftime('%Y-%m-%d')
   time_period['End'] = dt_end.strftime('%Y-%m-%d')
   return time_period
 
 def lambda_handler(event, context):
   client = boto3.client('ce', 'us-east-1')
-  time_period = get_time_period()
+  stat_type = os.environ.get('STAT_TYPE')
+  time_period = get_time_period(stat_type)
   resp = client.get_cost_and_usage(
     TimePeriod=time_period,
     Granularity='DAILY',
@@ -66,7 +88,7 @@ def lambda_handler(event, context):
     ]
   )
   p = normalize_result(resp)
-  notify_to_slack(p, time_period['Start'])
+  notify_to_slack(p, time_period, stat_type)
   print(json.dumps(p, indent=2))
 
 lambda_handler(None, None)
